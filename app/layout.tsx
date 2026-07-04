1b674ac83deae2ca933d567e784ed13aac583c5d
Almir Ishmukhametov <alimfyl@gmail.com>
2026-07-04 23:58:46 +0500

    всё еще исправление веток

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+import { Montserrat } from 'next/font/google';
+import './globals.css';
+
+const montserrat = Montserrat({
+  variable: '--font-montserrat',
+  subsets: ['latin', 'cyrillic'],
+});
+
+export const metadata: Metadata = {
+  title: 'Skypro Music',
+  description: 'Music service',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ru" className={montserrat.variable}>
+      <body>{children}</body>
+    </html>
+  );
+}
