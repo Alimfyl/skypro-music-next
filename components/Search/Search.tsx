90b82559a652e8d20aa4cc6f96f6ef6b8f62e517
Almir Ishmukhametov <alimfyl@gmail.com>
2026-07-04 20:31:28 +0500

    +search.css

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,11 +1,13 @@
+import styles from './Search.module.css';
+
 export function Search() {
     return (
-        <div className={'centerblock__search'}>
-              <svg className={'search__svg'}>
+        <div className={styles.centerblock__search}>
+              <svg className={styles.search__svg }>
                 <use xlinkHref="/img/icon/sprite.svg#icon-search"></use>
               </svg>
               <input
-                className={'search__text'}
+                className={styles.search__text}
                 type="search"
                 placeholder="Поиск"
                 name="search"
