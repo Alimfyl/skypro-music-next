4d9f4b57a6b80ade9141198abe21fb1864127cd0
Almir Ishmukhametov <alimfyl@gmail.com>
2026-07-04 10:48:33 +0500

    nav вынесен, начат search

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,14 @@
 import './page.css';
 import Image from 'next/image';
 import Link from 'next/link';
+import { Nav } from '@/components/Nav/Nav';
 
 export default function Home() {
   return (
     <div className={'wrapper'}>
       <div className={'container'}>
         <main className={'main'}>
-          <nav className={'main__nav'}>
-            <div className={'nav__logo'}>
-              {/*TODO: img –> Image*/}
-              <Image
-                width={250}
-                height={170}
-                className={'logo__image'}
-                src="/img/logo.png"
-                alt={'logo'}
-              />
-            </div>
-            <div className={'nav__burger'}>
-              <span className={'burger__line'}></span>
-              <span className={'burger__line'}></span>
-              <span className={'burger__line'}></span>
-            </div>
-            <div className={'nav__menu'}>
-              <ul className={'menu__list'}>
-                <li className={'menu__item'}>
-                  {/*TODO: a -> Link*/}
-                  <Link href="#" className={'menu__link'}>
-                    Главное
-                  </Link>
-                </li>
-                <li className={'menu__item'}>
-                  <Link href="#" className={'menu__link'}>
-                    Мой плейлист
-                  </Link>
-                </li>
-                <li className={'menu__item'}>
-                  <Link href="signin" className={'menu__link'}>
-                    Войти
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
+          <Nav />
           <div className={'centerblock'}>
             <div className={'centerblock__search'}>
               <svg className={'search__svg'}>
