10ed73b5f64d1526b59218666fa750b6c9ca901a
Almir Ishmukhametov <alimfyl@gmail.com>
2026-07-11 15:50:36 +0500

    переделка Track, чтобы клик по треку записывал его в Redux

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { StoreProvider } from '@/providers/StoreProvider';
 import type { Metadata } from 'next';
 import { Montserrat } from 'next/font/google';
 import './globals.css';
@@ -19,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru" className={montserrat.variable}>
-      <body>{children}</body>
+      <body>
+        <StoreProvider>{children}</StoreProvider>
+      </body>
     </html>
   );
 }
--- src/components/Track/Track.tsx
+++ src/components/Track/Track.tsx
@@ -1,48 +1,59 @@
+'use client';
+
 import Link from 'next/link';
+import { setCurrentTrack } from '@/store/features/playerSlice';
+import { useAppDispatch } from '@/store/hooks';
+import type { TrackType } from '@/data/tracks';
 import styles from './Track.module.css';
 
 type TrackProps = {
-    title: string;
-    titleSpan?: string;
-    author: string;
-    album: string;
-    time: string;
+  track: TrackType;
 };
-export function Track({ title, titleSpan, author, album, time }: TrackProps) {
-    return (
-                <div className={styles.playlist__item}>
-                  <div className={styles.playlist__track}>
-                    <div className={styles.track__title}>
-                      <div className={styles.track__titleImage}>
-                        <svg className={styles.track__titleSvg}>
-                          <use xlinkHref="/img/icon/sprite.svg#icon-note"></use>
-                        </svg>
-                      </div>
-                      <div className={styles.track__titleText}>
-                        <Link className={styles.track__titleLink} href="#">
-                          {title}
-                          {titleSpan && (<span className={styles.track__titleSpan}>{titleSpan}</span>)} 
-                        </Link>
-                      </div>
-                    </div>
-                    <div className={styles.track__author}>
-                      <Link className={styles.track__authorLink} href="#">
-                        {author}
-                      </Link>
-                    </div>
-                    <div className={styles.track__album}>
-                      <Link className={styles.track__albumLink} href="#">
-                        {album}
-                      </Link>
-                    </div>
-                    <div className={styles.track__time}>
-                      <svg className={styles.track__timeSvg}>
-                        <use xlinkHref="/img/icon/sprite.svg#icon-like"></use>
-                      </svg>
-                      <span className={styles.track__timeText}>{time}</span>
-                    </div>
-                  </div>
-                </div>
-         
-    )
+
+export function Track({ track }: TrackProps) {
+  const dispatch = useAppDispatch();
+
+  const handleTrackClick = () => {
+    dispatch(setCurrentTrack(track));
+  };
+
+  return (
+    <div className={styles.playlist__item} onClick={handleTrackClick}>
+      <div className={styles.playlist__track}>
+        <div className={styles.track__title}>
+          <div className={styles.track__titleImage}>
+            <svg className={styles.track__titleSvg}>
+              <use xlinkHref="/img/icon/sprite.svg#icon-note"></use>
+            </svg>
+          </div>
+          <div className={styles.track__titleText}>
+            <Link className={styles.track__titleLink} href="#">
+              {track.title}
+              {track.titleSpan && (
+                <span className={styles.track__titleSpan}>
+                  {track.titleSpan}
+                </span>
+              )}
+            </Link>
+          </div>
+        </div>
+        <div className={styles.track__author}>
+          <Link className={styles.track__authorLink} href="#">
+            {track.author}
+          </Link>
+        </div>
+        <div className={styles.track__album}>
+          <Link className={styles.track__albumLink} href="#">
+            {track.album}
+          </Link>
+        </div>
+        <div className={styles.track__time}>
+          <svg className={styles.track__timeSvg}>
+            <use xlinkHref="/img/icon/sprite.svg#icon-like"></use>
+          </svg>
+          <span className={styles.track__timeText}>{track.time}</span>
+        </div>
+      </div>
+    </div>
+  );
 }
