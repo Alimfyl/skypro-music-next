1711aa30041065461d374bb4f363ca47989adad7
Almir Ishmukhametov <alimfyl@gmail.com>
2026-07-16 20:31:24 +0500

    переделанны Track и Playlist

--- src/components/Playlist/Playlist.tsx
+++ src/components/Playlist/Playlist.tsx
@@ -6,18 +6,25 @@ export function Playlist() {
   return (
     <div className={styles.centerblock__content}>
       <div className={styles.content__title}>
-        <div className={styles.playlistTitle__col + ' ' + styles.col01}>Трек</div>
-        <div className={styles.playlistTitle__col + ' ' + styles.col02}>Исполнитель</div>
-        <div className={styles.playlistTitle__col + ' ' + styles.col03}>Альбом</div>
+        <div className={styles.playlistTitle__col + ' ' + styles.col01}>
+          Трек
+        </div>
+        <div className={styles.playlistTitle__col + ' ' + styles.col02}>
+          Исполнитель
+        </div>
+        <div className={styles.playlistTitle__col + ' ' + styles.col03}>
+          Альбом
+        </div>
         <div className={styles.playlistTitle__col + ' ' + styles.col04}>
           <svg className={styles.playlistTitle__svg}>
             <use xlinkHref="/img/icon/sprite.svg#icon-watch"></use>
           </svg>
         </div>
       </div>
+
       <div className={styles.content__playlist}>
         {tracks.map((track) => (
-          <Track key={track.id} track={track}/>
+          <Track key={track.id} track={track} playlist={tracks} />
         ))}
       </div>
     </div>
